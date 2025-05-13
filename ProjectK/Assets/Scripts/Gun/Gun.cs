a32c36f839149d79adaad523b1a09eeb8f6941df
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-05-13 11:09:50 +0900

    [Update] 트랜스폼 찾는 부분 함수로 분리

--- ProjectK/Assets/Scripts/Gun/Gun.cs
+++ ProjectK/Assets/Scripts/Gun/Gun.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform playerTransform;
     [SerializeField] private Transform fireTransform;
   
     private void Start()
     {
+        FindTransform();
+    }
+    
+    private void FindTransform()
+    {
+        //총알 생성될 위치 
         fireTransform = transform.Find("fireTransform");
 
+        //플레이어 위치
         Transform findTransform = transform;
         while (findTransform != null)
         {
@@ -19,10 +25,10 @@ public class Gun : MonoBehaviour
                 playerTransform = findTransform;
                 break;
             }
-            
+
             findTransform = findTransform.parent;
         }
-        if(playerTransform == null)
+        if (playerTransform == null)
         {
             Debug.LogError("플레이어 트랜스폼 찾지 못했음");
         }
