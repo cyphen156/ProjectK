769229f49b18fb7867da975da90125ac466b0a30
doncici77 <pkh2956@naver.com>
2025-05-13 15:03:59 +0900

    [Add] 플레이어 달리기 구현
    PlayerController.cs
    LeftShift 입력시 runSpeed를 기준으로 속도가 바뀜
    
    PlayerMove.cs
    변수명 수정

--- ProjectK/Assets/Scripts/Player/PlayerController.cs
+++ ProjectK/Assets/Scripts/Player/PlayerController.cs
@@ -2,25 +2,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("ÇÑ±Û")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float defaultSpeed;
+    [SerializeField] private float runSpeed;
+
     private PlayerMove playerMove;
     private float inputHorizontal;
     private float inputVertical;
 
     private void Awake()
     {
-        moveSpeed = 5.0f;
+        defaultSpeed = 5.0f;
+        runSpeed = 8.0f;
+        moveSpeed = defaultSpeed;
         playerMove = GetComponent<PlayerMove>();
     }
 
     private void Update()
     {
+        InputMove();
+    }
 
-
-
+    private void InputMove()
+    {
         inputHorizontal = Input.GetAxis("Horizontal");
         inputVertical = Input.GetAxis("Vertical");
 
-        playerMove.Move(inputHorizontal * Time.deltaTime * moveSpeed, inputVertical * Time.deltaTime * moveSpeed);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerMove.Move(inputHorizontal * Time.deltaTime * runSpeed, inputVertical * Time.deltaTime * runSpeed);
+        }
+        else
+        {
+            playerMove.Move(inputHorizontal * Time.deltaTime * moveSpeed, inputVertical * Time.deltaTime * moveSpeed);
+        }
     }
 }
