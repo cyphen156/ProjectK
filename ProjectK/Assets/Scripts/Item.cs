468a7e4b2ae0165dbd1e0a76be7176d2eea11cc3
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-05-16 16:32:57 +0900

    마스터데이터

---
+++ ProjectK/Assets/Scripts/Item.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Profiling;
+
+
+public enum ItemType
+{
+    None,
+    부착물,
+    소모품
+}
+
+public enum SubType
+{
+    None,
+    총알,
+    총구,
+    레일,
+    탄창,
+    회복,
+    스테미너,
+    설치,
+    투척
+}
+
+public enum UseType
+{
+    None,
+    장착,
+    회복,
+    스테미너,
+    설치,
+    투척
+}
+
+public enum Stat
+{
+    None,
+    Damage,
+    Rps,
+    Hp,
+    Stemina,
+    탄퍼짐,
+    ReloadTime,
+    AmmoSize
+}
+
+public class Item
+{
+    public int id;
+    public ItemType itemType;
+    public SubType subType;
+    public UseType useType;
+    public string name;
+    public Stat stat;
+    public int power;
+    public bool isBuff;
+    public float lifeTime;
+    public float damageRange;
+    public float throwMaxReach;
+
+    public Item(int inId, ItemType inItemType, SubType inSubType, UseType inUseType, string inName, Stat inStat, int inPower, bool inIsBuff = false, float inLifeTime = 0f, float inDamageRange = 0f, float inThrowMaxReach = 0f)
+    {
+        id = inId;
+        itemType = inItemType;
+        subType = inSubType;
+        useType = inUseType;
+        name = inName;
+        stat = inStat;
+        power = inPower;
+        isBuff = inIsBuff;
+        lifeTime = inLifeTime;
+        damageRange = inDamageRange;
+        throwMaxReach = inThrowMaxReach;
+    }
+
+    //깊은복사
+    public Item(Item inOrigin)
+    {
+        id = inOrigin.id;
+        itemType = inOrigin.itemType;
+        subType = inOrigin.subType;
+        useType = inOrigin.useType;
+        name = inOrigin.name;
+        stat = inOrigin.stat; 
+        power = inOrigin.power;
+        isBuff = inOrigin.isBuff;
+        lifeTime = inOrigin.lifeTime;
+        damageRange = inOrigin.damageRange;
+        throwMaxReach = inOrigin.throwMaxReach;
+    }
+
+}
---
+++ ProjectK/Assets/Scripts/MasterDataManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class MasterDataManager :MonoBehaviour
+{
+    public static MasterDataManager Instance;
+    private Dictionary<int, Item> masterItemDictionary;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            MakeMasterData();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void MakeMasterData()
+    {
+        masterItemDictionary = new();
+        Item compensator = new Item(1121, ItemType.부착물, SubType.총구, UseType.장착, "보정기", Stat.탄퍼짐, 30);
+        masterItemDictionary.Add(compensator.id, compensator);
+        Item treeBox = new Item(2472, ItemType.소모품, SubType.설치, UseType.설치, "나무상자", Stat.Hp, 100);
+        masterItemDictionary.Add(treeBox.id, treeBox);
+    }
+
+    public Dictionary<int, Item> GetMasterDataDic()
+    {
+        return masterItemDictionary;
+    }
+
+    public Item GetMasterItemData(int inId)
+    {
+        return masterItemDictionary[inId];
+    }
+}
