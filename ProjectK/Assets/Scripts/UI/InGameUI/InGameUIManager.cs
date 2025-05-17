dac5445539503fe4323417e799878c64464546b1
doncici77 <pkh2956@naver.com>
2025-05-17 19:34:58 +0900

    [Update] InGameUI 캔버스 오브젝트 분리
    1. 플레이어 프리팹의 인게임 UI 오브젝트 삭제
    2. 인게임UI 오브젝트 프리팹 생성
    3. InGameHpSlider가 플레이어 위치를 추적하게 수정

--- ProjectK/Assets/Scripts/UI/InGameUI/InGameUIManager.cs
+++ ProjectK/Assets/Scripts/UI/InGameUI/InGameUIManager.cs
@@ -3,28 +3,41 @@ using UnityEngine.UI;
 
 public class InGameUIManager : MonoBehaviour
 {
-    private PlayerStat playerStat;
+    public PlayerStat playerStat;
     private Slider hpSlider;
+    private RectTransform hpSliderRect;
+
+    private Transform targetPlayerTransform; // 추적할 대상 (플레이어 Transform)
+    private Vector3 offset; // 머리 위 위치 오프셋
+
+    private void Awake()
+    {
+        offset = new Vector3(0, 3f, 0);
+    }
 
     private void Start()
     {
-        playerStat = GetComponentInParent<PlayerStat>();
         hpSlider = transform.Find("InGameHpSlider").GetComponent<Slider>();
-
-        // hp바 초기화
         hpSlider.maxValue = 100f;
+        hpSliderRect = hpSlider.GetComponent<RectTransform>();
+
         if (playerStat != null)
         {
             hpSlider.value = playerStat.GetHP();
+            targetPlayerTransform = playerStat.transform;
         }
-
-        // 카메라 방향으로 바라보게 하는 코드
-        transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward);
     }
 
     private void Update()
     {
-        // 카메라 방향으로 바라보게 하는 코드
-        transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward);
+        TeackingPlayer();
+    }
+
+    private void TeackingPlayer()
+    {
+        // 월드 위치 + offset을 화면 좌표로 변환
+        Vector3 worldPosition = targetPlayerTransform.position + offset;
+        Vector3 screenPosition = Camera.main.WorldToScreenPoint(worldPosition);
+        hpSliderRect.position = screenPosition;
     }
 }
