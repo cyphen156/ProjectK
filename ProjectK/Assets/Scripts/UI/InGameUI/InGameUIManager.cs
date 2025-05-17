90e52de107388a955abe915c3620a012326e6517
doncici77 <pkh2956@naver.com>
2025-05-17 17:40:23 +0900

    [Add] 플레이어 HP 슬라이더 UI 연동
    1. 플레이어 프리펩 안에 InGameUI 캔버스 오브젝트 추가
    2. InGameUI 에 InGameHpSlider 추가
    3. PlayerUI 프리팹 수정
    4. InGameUIManager.cs 생성 및 슬라이더 연결하는 스크립트 작성

--- ProjectK/Assets/Scripts/UI/InGameUI/InGameUIManager.cs
+++ ProjectK/Assets/Scripts/UI/InGameUI/InGameUIManager.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameUIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerStat playerStat;
+    private Slider hpSlider;
+
+    private void Start()
     {
-        
+        playerStat = GetComponentInParent<PlayerStat>();
+        hpSlider = transform.Find("InGameHpSlider").GetComponent<Slider>();
+
+        // hp바 초기화
+        hpSlider.maxValue = 100f;
+        if (playerStat != null)
+        {
+            hpSlider.value = playerStat.GetHP();
+        }
+
+        // 카메라 방향으로 바라보게 하는 코드
+        transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // 카메라 방향으로 바라보게 하는 코드
+        transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward);
     }
 }
