59e667cbae040c581ce22a27ee71078f830c2017
doncici77 <pkh2956@naver.com>
2025-05-18 17:48:30 +0900

    [Add] 드롭박스 인벤토리 PopUp UI 배치
    1. PlayerUI 오브젝트에 DropBoxPanel 추가
    2. DropBox_ItemSlot1 프리펩 추가 및 배치
    3. PlayerUI 프리팹 수정
    4. 각 버튼에 이벤트 리스너를 등록해서 각 버튼의 정보를 가져오는 코드 추가
    5. Crosshair 이미지 Raycast Target 비활성화
    6. dropBoxPanel이 활성화 비활성화 되는 함수 추가

--- ProjectK/Assets/Scripts/UI/PlayerUI/PlayerUIManager.cs
+++ ProjectK/Assets/Scripts/UI/PlayerUI/PlayerUIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,12 +7,33 @@ public class PlayerUIManager : MonoBehaviour
     public PlayerStat playerStat;
 
     [SerializeField] private Transform crosshairTransform;
+    [SerializeField] private Button[] DropboxSlotButtons;
     private Slider hpSlider;
     private Slider staminaSlider;
+    private GameObject dropBoxPanelObj;
+    private bool isOnDropBoxPanel;
+
+    private void Awake()
+    {
+        isOnDropBoxPanel = false;
+    }
 
     private void Start()
     {
         crosshairTransform = transform.Find("Crosshair");
+
+        StartSettingHUDUI();
+
+        StartSettingDropBoxUI();
+    }
+
+    private void Update()
+    {
+        crosshairTransform.position = Input.mousePosition;
+    }
+
+    private void StartSettingHUDUI()
+    {
         hpSlider = transform.Find("PlayerHUDPanel/HpSlider").GetComponent<Slider>();
         staminaSlider = transform.Find("PlayerHUDPanel/StaminaSlider").GetComponent<Slider>();
 
@@ -29,8 +51,39 @@ public class PlayerUIManager : MonoBehaviour
         }
     }
 
-    void Update()
+    private void StartSettingDropBoxUI()
     {
-        crosshairTransform.position = Input.mousePosition;
+        // 버튼에 리스너 등록
+        for (int i = 0; i < DropboxSlotButtons.Length; i++)
+        {
+            int index = i;
+            Button button = DropboxSlotButtons[i];
+            button.onClick.AddListener(delegate {
+                DropBoxSlotClick(index, button);
+            });
+        }
+
+        dropBoxPanelObj = transform.Find("DropBoxPanel").gameObject;
+        dropBoxPanelObj.SetActive(false);
+    }
+
+    private void OnOffDorpBoxUI()
+    {
+        if(isOnDropBoxPanel == true)
+        {
+            dropBoxPanelObj?.SetActive(false);
+            isOnDropBoxPanel = false;
+        }
+        else
+        {
+            dropBoxPanelObj?.SetActive(true);
+            isOnDropBoxPanel = true;
+        }
+    }
+
+    private void DropBoxSlotClick(int inIndex, Button inDropBoxIndex)
+    {
+        Debug.Log($"[{inIndex}]번 슬롯 클릭됨");
+        Debug.Log("클릭한 오브젝트 이름: " + inDropBoxIndex.gameObject.name);
     }
 }
