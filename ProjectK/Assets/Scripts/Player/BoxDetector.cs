6cc9285872ad7b5d3c5fa710de6194065962a340
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-05-19 17:33:23 +0900

    [Add] 박스디텍터 cs 1. BoxDetector 스크립트  - 트리거 온, 아웃시 범위박스 리스트에 추가  - GetJNearest()에서 범위 내 박스 중 가장 가까운 박스 반환

---
+++ ProjectK/Assets/Scripts/Player/BoxDetector.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BoxDetector : MonoBehaviour
+{
+    private List<DropBox> inRangeBoxList; //범위 안에 들어온 상자들
+
+    private void Awake()
+    {
+        inRangeBoxList = new();
+    }
+
+    public DropBox GetNearestBox()
+    {
+        DropBox detectedBox = null;
+        float minDisstance = float.MaxValue;
+        //상자리스트들 중 가장 가까운애를 반환
+        for (int i = 0; i < inRangeBoxList.Count; i++)
+        {
+            float curDistance = Vector3.Distance(inRangeBoxList[i].transform.position, transform.position);
+            if (curDistance < minDisstance)
+            {
+                detectedBox = inRangeBoxList[i];
+                minDisstance = curDistance;
+            }
+        }
+        return detectedBox;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       // Debug.Log("박스디텍터에서 출입" + other.gameObject.name);
+        DropBox box = other.gameObject.GetComponent<DropBox>();
+        if(box == null)
+        {
+            return;
+        }
+        inRangeBoxList.Add(box);
+    }
+
+ 
+    private void OnTriggerExit(Collider other)
+    {
+      //  Debug.Log("박스디텍터에서 반출" + other.gameObject.name);
+        DropBox box = other.gameObject.GetComponent<DropBox>();
+        if (box == null)
+        {
+            return;
+        }
+        inRangeBoxList.Remove(box);
+    }
+}
