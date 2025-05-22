c823278d1d8f11cb1c4ec10eeffc9060211f76b7
YiYop <wjdrbsdl1011@naver.com>
2025-05-22 21:18:01 +0900

    [Update] 재활용위한 총알 세팅 1. 활성화 여부 NetworkVariable로 동기화 2. 활성화 여부 변경시 이벤트로 SetActive 진행 3. 삭제 대신 비활성화 호출

--- ProjectK/Assets/Scripts/Gun/Bullet.cs
+++ ProjectK/Assets/Scripts/Gun/Bullet.cs
@@ -1,30 +1,57 @@
 using UnityEngine;
+using Unity.Netcode;
 
-public class Bullet : MonoBehaviour
+public class Bullet : NetworkBehaviour
 {
     private float defaultLifeTime;
     private float lifeTime;
     private float speed;
     private float damage;
     private Vector3 direction;
+    private NetworkVariable<bool> isActive;
 
     private void Awake()
     {
-        //변수 초기값 
+        isActive = new NetworkVariable<bool>();
+        isActive.Value = false;
+        gameObject.SetActive(isActive.Value);
+        isActive.OnValueChanged += OnActiveValueChange;
         defaultLifeTime = 5.0f;
         lifeTime = defaultLifeTime;
         speed = 14.0f;
         damage = 30f;
+
+
+
     }
 
     public void SetDirection(Vector3 inDirection)
     {
         //총에서 만들어서 호출
         direction = inDirection;
+        lifeTime = defaultLifeTime;
+        isActive.Value = true;
     }
 
     private void Update()
     {
+        if(IsHost == false)
+        {
+            Debug.Log("호스트가 아니다");
+        
+        }
+
+        if (IsServer == false)
+        {
+            Debug.Log("서버가 아니다");
+    
+        }
+
+        if(IsClient == false)
+        {
+            Debug.Log("클라아님");
+        }
+
         Move();
         CountLifeTime();
     }
@@ -37,10 +64,16 @@ public class Bullet : MonoBehaviour
     private void CountLifeTime()
     {
         lifeTime -= Time.deltaTime;
-        if(lifeTime <= 0)
+        if (lifeTime <= 0)
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            InActive();
         }
+    }
+
+    private void OnActiveValueChange(bool inPreActive, bool inActive)
+    {
+        gameObject.SetActive(inActive);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -49,7 +82,13 @@ public class Bullet : MonoBehaviour
         if (takeDamageObj != null)
         {
             takeDamageObj.TakeDamage(damage);
-            Destroy(gameObject);
+            InActive();
         }
+    }
+
+    public void InActive()
+    {
+        isActive.Value = false;
+        BulletPool.Instance.Recycle(this);
     }
 }
