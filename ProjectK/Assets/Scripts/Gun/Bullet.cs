4d095ab3bd7ce3ee34b0c3287d511db90e2fc08f
wjdrbsdl <wjdrbsdl1011@naver.com>
2025-05-23 11:43:42 +0900

    [Feature] 활성 비활성 동기화

--- ProjectK/Assets/Scripts/Gun/Bullet.cs
+++ ProjectK/Assets/Scripts/Gun/Bullet.cs
@@ -8,24 +8,58 @@ public class Bullet : NetworkBehaviour
     private float speed;
     private float damage;
     private Vector3 direction;
+    private NetworkVariable<bool> isActive = new NetworkVariable<bool>(false);
 
     private void Awake()
     {
-        //변수 초기값 
+        gameObject.SetActive(isActive.Value);
         defaultLifeTime = 5.0f;
         lifeTime = defaultLifeTime;
         speed = 14.0f;
         damage = 30f;
+
+
+
+    }
+    public override void OnNetworkSpawn()
+    {
+        isActive.OnValueChanged += OnActiveValueChange;
     }
 
-    public void SetDirection(Vector3 inDirection)
+    public void SetDirection(Vector3 inSpawnPosition, Vector3 inDirection)
     {
         //총에서 만들어서 호출
+        transform.position = inSpawnPosition;
         direction = inDirection;
+        lifeTime = defaultLifeTime;
+        ActiveRpc(true);
+    }
+
+    [Rpc(SendTo.Everyone)]
+    private void ActiveRpc(bool inActive)
+    {
+        gameObject.SetActive(inActive);
     }
 
     private void Update()
     {
+        if(IsHost == false)
+        {
+            Debug.Log("호스트가 아니다");
+        
+        }
+
+        if (IsServer == false)
+        {
+            Debug.Log("서버가 아니다");
+    
+        }
+
+        if(IsClient == false)
+        {
+            Debug.Log("클라아님");
+        }
+
         Move();
         CountLifeTime();
     }
@@ -38,10 +72,17 @@ public class Bullet : NetworkBehaviour
     private void CountLifeTime()
     {
         lifeTime -= Time.deltaTime;
-        if(lifeTime <= 0)
+        if (lifeTime <= 0)
         {
-            gameObject.GetComponent<NetworkObject>().Despawn();
+            //Destroy(gameObject);
+            InActive();
         }
+    }
+
+    private void OnActiveValueChange(bool inPreActive, bool inActive)
+    {
+        Debug.Log("바뀐다");
+        gameObject.SetActive(inActive);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -57,6 +98,17 @@ public class Bullet : NetworkBehaviour
                 gameObject.GetComponent<NetworkObject>().Despawn();
             }
         }
+    }
+
+    public void InActive()
+    {
+        if(IsHost == false)
+        {
+            Debug.Log("비활성화는 서버만보여주기");
+            return;
+        }
+        ActiveRpc(false);
+        BulletPool.Instance.Recycle(this);
     }
 
 }
