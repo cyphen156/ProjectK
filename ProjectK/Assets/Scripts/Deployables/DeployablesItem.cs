755e081616b42a9249a9c2acce184e61b9b8422c
LJH0521-91 <anrocksa@gmail.com>
2025-05-23 17:52:39 +0900

    [Add]설치형 프리팹 스크립트
    프리팹 안 내용없음

---
+++ ProjectK/Assets/Scripts/Deployables/DeployablesItem.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class DeployablesItem : MonoBehaviour
+{
+    [Header("Barricade Stats")]
+    [SerializeField] private float maxHp = 300f;
+    [SerializeField] private float maxLifeTime = 20f; 
+}
