6848865a4cd6d7fc8eaa581fb262104a419d7743
YiYop <wjdrbsdl1011@naver.com>
2025-05-25 13:25:56 +0900

    [Add] 총알 풀 1. BullPool 스크립트 추가  - 서버 시작시 정해진 숫자만큼 불렛 생성 및 스폰, 비활성화  - 재활용 함수  - 가져오기 함수

--- ProjectK/Assets/Scripts/Gun/BulletPool.cs
+++ ProjectK/Assets/Scripts/Gun/BulletPool.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 public class BulletPool : NetworkBehaviour
 {
     public static BulletPool Instance;
-    public int MakeCount = 50;
+    private int makeCount;
     [SerializeField] private Bullet bulletPrefab;
     private Queue<Bullet> pool;
 
     private void Awake()
     {
+        makeCount = 30;
         Instance = this;
     }
 
@@ -22,32 +21,40 @@ public class BulletPool : NetworkBehaviour
 
     private void MakePoolObject()
     {
-        Debug.Log("풀만들기 시작");
+       // Debug.Log("풀만들기 시작");
         if(IsServer == false)
         {
             return;
         }
-        Debug.Log("난 서버");
         pool = new() ;
-        for (int i = 0; i < MakeCount; i++)
+        MakeBullet();
+
+    }
+
+    private void MakeBullet()
+    {
+        for (int i = 0; i < makeCount; i++)
         {
             Bullet bullet = Instantiate(bulletPrefab, transform);
             bullet.GetComponent<NetworkObject>().Spawn();
+            bullet.ControlActive(false);
             pool.Enqueue(bullet);
         }
-
     }
 
     public void Recycle(Bullet inBullet)
     {
-        
         pool.Enqueue(inBullet);
-        Debug.Log("재활용 " + pool.Count);
+       // Debug.Log("재활용 " + pool.Count);
     }
 
     public Bullet GetBullet()
     {
-        Debug.Log(pool.Count + "에서 빼기");
+      //  Debug.Log(pool.Count + "에서 빼기");
+        if(pool.Count == 1)
+        {
+            MakeBullet();
+        }
         return pool.Dequeue();
     }
 }
